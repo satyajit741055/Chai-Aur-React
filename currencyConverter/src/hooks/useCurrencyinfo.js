ee5d2327913c961354888b9f40f3ca2787c427b5
satyajit741055 <satyajit7410@gmail.com>
2025-05-14 22:03:04 +0530

    input Componenet

--- currencyConverter/src/hooks/useCurrencyinfo.js
+++ currencyConverter/src/hooks/useCurrencyinfo.js
@@ -1,13 +1,14 @@
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
-function useCurrencyInfo(currency){
-    const [ data,setData ] = useState();
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({});
 
-    useEffect(()=>(
+    useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
-        .then((res)=>res.json())
-        .then((res)=>setData(res[currency]))
-    ),[currency])
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]))
+    }, [currency])
+    console.log(data)
 
     return data;
 }
